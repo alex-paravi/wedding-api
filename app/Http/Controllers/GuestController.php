1daf13004142d286712baf041a57c54ca8994efe
alexparavi <alexparavi@gmail.com>
2026-06-11 17:15:18 +0400

    Fix table name mapping and test with query params

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Guest;
+
+class GuestController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->only(['name', 'is_confirmed']);
+        $guest = Guest::create($data);
+        return response()->json($guest, 201);
+    }
+}
