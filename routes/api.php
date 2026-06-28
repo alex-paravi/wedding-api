70905db9f391bd194faae8c96371d215e5056281
alexparavi <alexparavi@gmail.com>
2026-06-28 18:41:50 +0400

    feat: implement token authentification using laravel sanctum and protect guest routes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::post('/guests', [GuestController::class, 'store']);
-Route::get('/guests', [GuestController::class, 'index']);
-Route::get('/guests/{guest}', [GuestController::class, 'show']);
-Route::patch('/guests/{guest}', [GuestController::class, 'update']);
-Route::delete('/guests/{guest}', [GuestController::class, 'destroy']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/guests', [GuestController::class, 'store']);
+    Route::get('/guests', [GuestController::class, 'index']);
+    Route::get('/guests/{guest}', [GuestController::class, 'show']);
+    Route::patch('/guests/{guest}', [GuestController::class, 'update']);
+    Route::delete('/guests/{guest}', [GuestController::class, 'destroy']);
+});
