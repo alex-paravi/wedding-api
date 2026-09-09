24691467446f07c194adc00e91bc2647fb8a760f
alexparavi <alexparavi@gmail.com>
2026-09-09 13:49:17 +0400

    chore: fix PHPStan property errors in resources via @mixin annotations

--- app/Http/Resources/GuestResource.php
+++ app/Http/Resources/GuestResource.php
@@ -5,11 +5,14 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin \App\Models\Guest
+ */
 class GuestResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
-     *
+     * 
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
