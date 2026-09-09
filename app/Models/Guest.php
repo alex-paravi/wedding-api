9220258bb5e831e9e9a10fe90f5ec4b84718ad72
alexparavi <alexparavi@gmail.com>
2026-09-09 13:40:14 +0400

    chore: add PHPDoc property annotations to models via ide-helper

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -7,6 +7,40 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $name
+ * @property string|null $phone
+ * @property string $side
+ * @property string $category
+ * @property string $status
+ * @property int|null $table_id
+ * @property string|null $invitation_token
+ * @property string|null $dietary_preferences
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Table|null $table
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\GuestFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest visibleTo(\App\Models\User $user)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereDietaryPreferences($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereInvitationToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereSide($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereTableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Guest extends Model
 {
     use HasFactory, HasOwner;
--- app/Models/Table.php
+++ app/Models/Table.php
@@ -8,6 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property int $capacity
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Guest> $guests
+ * @property-read int|null $guests_count
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\TableFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table visibleTo(\App\Models\User $user)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table whereCapacity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Table whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Table extends Model
 {
     use HasFactory, HasOwner;
